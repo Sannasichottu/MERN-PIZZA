1acfb9c0fa0b38f767df1efc29335a34ef4fcc7d
sannasi chottu <sannasichottu@gmail.com>
2022-02-27 13:55:37 +0530

    pizzadata & homepage

---
+++ pizza-delivery/src/pizzasdata.js
@@ -0,0 +1,90 @@
+const pizzas = [
+  {
+    name: "PEPPER BARBECUE CHICKEN",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    description: "Pepper Barbecue Chicken I Cheese",
+  },
+  {
+    name: "CHICKEN SAUSAGE",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Chicken_Sausage.jpg",
+    description: "Chicken Sausage I Cheese",
+  },
+  {
+    name: "NON VEG SUPREME",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Non-Veg_Supreme.jpg",
+    description:
+      "Bite into supreme delight of Black Olives, Onions, Grilled Mushrooms, Pepper BBQ Chicken, Peri-Peri Chicken, Grilled Chicken Rashers",
+  },
+  {
+    name: "CHICKEN GOLDEN DELIGHT",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Chicken_Golden_Delight.jpg",
+    description:
+      "Mmm! Barbeque chicken with a topping of golden corn loaded with extra cheese. Worth its weight in gold!",
+  },
+  {
+    name: "PEPPY PANEER",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+    description:
+      "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!",
+  },
+  {
+    name: "PANEER MAKHANI",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "veg",
+    image: "https://www.dominos.co.in/files/items/Paneer_Makhni.jpg",
+    description: "Paneer and Capsicum on Makhani Sauce",
+  },
+];
+export default pizzas;
